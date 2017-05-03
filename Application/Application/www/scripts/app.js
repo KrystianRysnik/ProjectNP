d09a0f9b506a067034da203390b50c5fec6d3994
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2017-05-03 13:55:59 +0200

    Dodano szablon dla ng-view

---
+++ Application/Application/www/scripts/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: "views/login.html"
+    })
+    .when('/register', {
+        templateUrl: "views/register.html"
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
