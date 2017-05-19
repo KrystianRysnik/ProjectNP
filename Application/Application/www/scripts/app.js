a15d2d49b76473831d9760d988858c370fd847fa
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2017-05-20 01:45:29 +0200

    Dodano polaczenie z baza + przejecie wybranego salonu dla view/salon.html

--- Application/Application/www/scripts/app.js
+++ Application/Application/www/scripts/app.js
@@ -160,3 +160,10 @@ app.controller("accountCtrl", ["$scope", "Auth", function($scope, Auth) {
   }
 }]);
 
+// Salon Controller
+app.controller("salonCtrl", ["$scope", "$location", "Auth", "$firebaseObject", function($scope, $location, Auth, $firebaseObject) {
+  var activeSalon = $location.search().activeSalon;
+
+  var ref = firebase.database().ref();
+  $scope.salon = $firebaseObject(ref.child('pl_salon').child(activeSalon));
+}])
