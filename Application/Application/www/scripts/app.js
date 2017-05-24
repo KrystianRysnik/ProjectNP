b80ee92ff848abf8dc67876039297409b8d3684d
studentUTP <Ares.genesis@gmail.com>
2017-05-24 20:28:12 +0200

    Zmiana lokalizacji kontrolera authCtrl

--- Application/Application/www/scripts/app.js
+++ Application/Application/www/scripts/app.js
@@ -70,27 +70,7 @@ app.factory("Auth", ["$firebaseAuth", function ($firebaseAuth) {
       return $firebaseAuth();
 }]);
 
-// Auth Controller
-app.controller("AuthCtrl", ["$scope", "Auth", "$firebaseObject",  function($scope, Auth, $firebaseObject) {
-    var ref = firebase.database().ref();
-    $scope.salons = $firebaseObject(ref.child('pl_salon'));
 
-    $scope.auth = Auth;
-    $scope.auth.$onAuthStateChanged(function(firebaseUser) {
-        // Wyswietlenie informacji o uzytkowniku np. {{ firebaseUser.email }}
-        $scope.firebaseUser = firebaseUser;
-    });
-    $scope.signOut = function() {
-    Auth.$signOut().then(function() {
-      location.reload();
-      console.log("Pomyślnie wylogowano.");
-    }).catch(function(error) {
-      console.error(error);
-    })
-
-  }
-    
-}]);
 
 // Login Controller
 app.controller("loginCtrl", ["$timeout", "$scope", "Auth", function ($timeout, $scope, Auth) {
---
+++ Application/Application/www/scripts/controllers/authCtrl.js
@@ -0,0 +1,21 @@
+// Auth Controller
+app.controller("AuthCtrl", ["$scope", "Auth", "$firebaseObject", function ($scope, Auth, $firebaseObject) {
+    var ref = firebase.database().ref();
+    $scope.salons = $firebaseObject(ref.child('pl_salon'));
+
+    $scope.auth = Auth;
+    $scope.auth.$onAuthStateChanged(function (firebaseUser) {
+        // Wyswietlenie informacji o uzytkowniku np. {{ firebaseUser.email }}
+        $scope.firebaseUser = firebaseUser;
+    });
+    $scope.signOut = function () {
+        Auth.$signOut().then(function () {
+            location.reload();
+            console.log("Pomyślnie wylogowano.");
+        }).catch(function (error) {
+            console.error(error);
+        })
+
+    }
+
+}]);
