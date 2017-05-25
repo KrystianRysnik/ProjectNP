1ca7ff03caf4e56cbdf3ee1fc11a8f69e299e596
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2017-05-25 17:44:24 +0200

    Dodano funkcje wyswietlenia godzin dla rezerwacji

--- Application/Application/www/scripts/controllers/salonCtrl.js
+++ Application/Application/www/scripts/controllers/salonCtrl.js
@@ -9,6 +9,31 @@ app.controller("salonCtrl", ["$scope", "$location", "Auth", "$firebaseObject", f
         $scope.activeService = event.target.id;
         $scope.showBook = 'true';
         $scope.now = new Date();
+        $scope.start = $scope.salon.hourStart;
+        $scope.stop = $scope.salon.hourEnd;
+
+        $scope.getTimeArray = function(start, stop, interval){
+            var result = [];
+            if(interval == '0')
+                return result;
+
+            function toMinutes(time){
+                var pair = time.split(':');
+                return pair[0] * 60 + 1 * pair[1];
+            }
+
+            function toString(minutes){
+                var hours = Math.floor(minutes / 60);
+                var min = (minutes - hours * 60);
+                min = min > 9 ? min : '0' + min;
+                return hours + ':' + min;
+            }
+
+            for(var i = toMinutes(start); i <= toMinutes(stop); i += 1 * interval)
+                result.push(toString(i))
+
+            return result;
+        }
     }
 
     $scope.prevDay = function() {
