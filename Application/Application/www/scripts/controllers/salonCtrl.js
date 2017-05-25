b1519f92baa734cb00510316a047167cbbc6a203
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2017-05-25 17:42:09 +0200

    Dodano okno rezerwacji i wprowadzono poprawki

--- Application/Application/www/scripts/controllers/salonCtrl.js
+++ Application/Application/www/scripts/controllers/salonCtrl.js
@@ -1,7 +1,12 @@
 // Salon Controller
-app.controller("salonCtrl", ["$scope", "$location", "Auth", "$firebaseObject", function ($scope, $location, Auth, $firebaseObject) {
-    var activeSalon = $location.search().activeSalon;
+app.controller("salonCtrl", ["$scope", "$location", "Auth", "$firebaseObject", function($scope, $location, Auth, $firebaseObject) {
+    $scope.activeSalon = $location.search().activeSalon;
 
     var ref = firebase.database().ref();
-    $scope.salon = $firebaseObject(ref.child('pl_salon').child(activeSalon));
+    $scope.salon = $firebaseObject(ref.child('pl_salon').child($scope.activeSalon));
+
+    $scope.booking = function(event) {
+        $scope.activeService = event.target.id;
+        $scope.showBook = 'true';
+    }
 }])
