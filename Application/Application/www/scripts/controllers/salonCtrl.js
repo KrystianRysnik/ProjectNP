de66444c0ca17900813afef4bc4432025a6fd56f
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2017-05-25 17:42:58 +0200

    Dodano wybor daty w oknie rezerwacji

--- Application/Application/www/scripts/controllers/salonCtrl.js
+++ Application/Application/www/scripts/controllers/salonCtrl.js
@@ -8,5 +8,14 @@ app.controller("salonCtrl", ["$scope", "$location", "Auth", "$firebaseObject", f
     $scope.booking = function(event) {
         $scope.activeService = event.target.id;
         $scope.showBook = 'true';
+        $scope.now = new Date();
+    }
+
+    $scope.prevDay = function() {
+        $scope.now = new Date(($scope.now.getTime()-(1*24*60*60*1000)))
+    }
+
+    $scope.nextDay = function() {
+        $scope.now = new Date(($scope.now.getTime()+(1*24*60*60*1000)))
     }
 }])
