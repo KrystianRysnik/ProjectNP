aaf9b1adaa968d31fa2f0be9b383e46170dc8af2
PiotrLipkowski <Piotrek789150@gmail.com>
2017-05-26 20:15:29 +0200

    Dodano mozliwosc usuniecia konta

--- Application/Application/www/scripts/controllers/accCtrl.js
+++ Application/Application/www/scripts/controllers/accCtrl.js
@@ -1,4 +1,21 @@
 // Account Controller
 app.controller("accountCtrl", ["$scope", "Auth", function ($scope, Auth) {
-
+    
+$scope.usun = function(){
+   var odp = confirm("Czy na pewno chcesz usunąć użytkownika ?");
+    if(odp == true){
+        
+               Auth.$deleteUser().then(function() {
+                   console.log('Deleted');
+                   location.reload();
+      }).catch(function(error) {
+                   console.log("Error");
+      });
+                   
+    }
+    else{
+        alert("Dobrze, że z nami pozostajesz");
+    }
+};
+    
 }]);
